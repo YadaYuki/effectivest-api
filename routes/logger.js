c7ab1268689e917c4e961347f1e9adf5a607783f
YadaYuki <yada.yuki@fuji.waseda.jp>
2020-04-08 15:00:48 +0900

    add file and revised for production

--- routes/logger.js
+++ routes/logger.js
@@ -1,12 +1,18 @@
 const log4js = require("log4js");
-if(process.env.NODE_ENV==="production"){
-    var date = new Date();
-    log4js.configure({
-    appenders: { cheese: { type: 'file', filename: "logger-"+date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate()+".log" } },
-    categories: { default: { appenders: [''], level: 'error' } }
+if (process.env.NODE_ENV === "production") {
+  var date = new Date();
+  log4js.configure({
+    appenders: {
+      production:
+        { type: 'file', filename: "./log/logger-" + date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + ".log" },
+    },
+    categories: {
+      default: { appenders: ['production'], level: 'info' }
+    }
   });
+  loggerjs = log4js.getLogger("production");
 }
-  loggerjs = log4js.getLogger();
-  loggerjs.level = 'debug'
-  loggerjs.debug("startApp");
-  module.exports = loggerjs;
+loggerjs = log4js.getLogger();
+loggerjs.level = 'debug'
+loggerjs.debug("startApp");
+module.exports = loggerjs;
